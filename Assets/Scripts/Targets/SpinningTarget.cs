cca45cfbbefb0cabe4775718b90c42c719ed0313
Nicodragon25 <nicolasariellaure@gmail.com>
2024-05-14 14:00:53 -0300

    Rename targets scripts

--- Assets/Scripts/Targets/SpinningTarget.cs
+++ Assets/Scripts/Targets/SpinningTarget.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetThatSpins : Target
+public class SpinningTarget : Target
 {
     protected override IEnumerator GotShotCoroutine()
     {
