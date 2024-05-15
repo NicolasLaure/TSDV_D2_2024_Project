5e4d592dc091b0d2c02d6a1d4e81e40a00063051
Nicodragon25 <nicolasariellaure@gmail.com>
2024-05-15 20:10:33 -0300

    Add additive and remove scene methods to loader

--- Assets/Scripts/SceneManagement/Loader.cs
+++ Assets/Scripts/SceneManagement/Loader.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class Loader : MonoBehaviour
 {
     public void ChangeScene(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
+    }
+
+    public void AddScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex, LoadSceneMode.Additive);
+    }
+    public void RemoveScene(int buildIndex)
+    {
+        SceneManager.UnloadSceneAsync(buildIndex);
     }
 }
