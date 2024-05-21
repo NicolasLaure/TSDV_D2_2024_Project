691c404124599746a99e2c0f8d0ba391b3d86312
Nicodragon25 <nicolasariellaure@gmail.com>
2024-05-21 17:43:42 -0300

    Add moving target

---
+++ Assets/Scripts/Targets/MovingTarget.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform minXObject;
+    [SerializeField] private Transform maxXObject;
+
+    private Transform currentTargetPos;
+    private Vector3 dir;
+
+    private void Start()
+    {
+        currentTargetPos = minXObject;
+        SetDir();
+    }
+    void Update()
+    {
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+        if (Mathf.Abs(transform.position.x - currentTargetPos.position.x) < 0.5f)
+            SwitchDir();
+    }
+
+    private void SetDir()
+    {
+        dir = new Vector3(currentTargetPos.position.x - transform.position.x, 0, 0);
+    }
+    private void SwitchDir()
+    {
+        currentTargetPos = currentTargetPos == minXObject ? maxXObject : minXObject;
+        SetDir();
+    }
+}
