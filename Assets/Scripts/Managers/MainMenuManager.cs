ab76901e82acfd081fb73a9551e564431a2ec976
Nicodragon25 <nicolasariellaure@gmail.com>
2024-05-22 16:25:38 -0300

    Add tutorial scene loading on first play

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    private static bool isFirstRun = true;
+
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        isFirstRun = true;
+    }
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void StartGame()
+    {
+        if (isFirstRun)
+            Loader.ChangeScene(1);
+        else
+            Loader.ChangeScene(2);
+
+        isFirstRun = false;
     }
 }
