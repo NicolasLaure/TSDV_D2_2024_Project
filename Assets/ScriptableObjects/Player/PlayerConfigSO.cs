e2ee29f51fe329af13ddd4348df45c38b8f83b9e
NicolasLaure <nicolasariellaure@gmail.com>
2024-05-27 11:42:18 -0300

    Add simple options panel to main menu

--- Assets/ScriptableObjects/Player/PlayerConfigSO.cs
+++ Assets/ScriptableObjects/Player/PlayerConfigSO.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New PlayerConfigSO", menuName = "ScriptableObjects/Player", order = 0)]
 public class PlayerConfigSO : ScriptableObject
 {
-    [Range(0, 1)]
+    [Range(0.01f, 0.5f)]
     [SerializeField] public float lookSensitivity;
+
+    private float min = 0.01f;
+    private float max = 0.5f;
+    public void SetSensitivity(float sens)
+    {
+        lookSensitivity = Mathf.Clamp(sens, min, max);
+    }
 }
