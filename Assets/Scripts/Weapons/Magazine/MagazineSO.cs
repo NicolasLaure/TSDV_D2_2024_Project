445a21d52261f7064b1ec6964395a8c3c0dbeaed
NicolasLaure <nicolasariellaure@gmail.com>
2024-06-05 10:33:48 -0300

    Add magazine scriptable objects

---
+++ Assets/Scripts/Weapons/Magazine/MagazineSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Magazine", menuName = "Weapons/Magazine", order = 0)]
+public class MagazineSO : ScriptableObject
+{
+    [SerializeField] private int size = 0;
+    [HideInInspector] public int Size { get { return size; } }
+}
