dd17a5fd310705259fafa0ba9d7a661dda13e3d8
NicolasLaure <nicolasariellaure@gmail.com>
2024-06-05 09:55:09 -0300

    Update asset menu for save scriptable object

--- Assets/Scripts/GameState/GameSaveSO.cs
+++ Assets/Scripts/GameState/GameSaveSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New GameSave", menuName = "ScriptableObjects/SaveSystem", order = 0)]
+[CreateAssetMenu(fileName = "New GameSave", menuName = "ScriptableObjects/SaveSystem/New Save", order = 0)]
 public class GameSaveSO : ScriptableObject
 {
     public bool wasTutorialFinished = false;   
