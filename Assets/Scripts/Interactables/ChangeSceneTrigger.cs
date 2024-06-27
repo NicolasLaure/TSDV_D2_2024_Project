2b6a199e08bad3426656118bca411e2f4e7c59ab
Nicodragon25 <nicolasariellaure@gmail.com>
2024-06-27 18:47:59 -0300

    Add Change scene to training track

---
+++ Assets/Scripts/Interactables/ChangeSceneTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneTrigger : MonoBehaviour
+{
+    [SerializeField] string nextSceneName;
+    private void OnTriggerEnter(Collider other)
+    {
+        Loader.ChangeScene(nextSceneName);
+    }
+}
--- Assets/Scripts/SceneManagement/Loader.cs
+++ Assets/Scripts/SceneManagement/Loader.cs
@@ -2,16 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 public class Loader : MonoBehaviour
 {
+    [SerializeField] private List<string> scenesInBuildNames = new List<string>();
     public static void ChangeScene(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
     }
 
+    public static void ChangeScene(string name)
+    {
+        Scene nextScene = SceneManager.GetSceneByName(name);
+
+        if (nextScene != null)
+            SceneManager.LoadScene(name, LoadSceneMode.Single);
+        else
+            Debug.LogError($"There is no scene with {name} name in build");
+    }
+
     public void AddScene(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex, LoadSceneMode.Additive);
+    }
+
+    public static void AddScene(string name)
+    {
+        Scene nextScene = SceneManager.GetSceneByName(name);
+
+        if (nextScene != null)
+            SceneManager.LoadScene(name, LoadSceneMode.Additive);
+        else
+            Debug.LogError($"There is no scene with {name} name in build");
     }
     public void RemoveScene(int buildIndex)
     {
@@ -21,5 +43,16 @@ public class Loader : MonoBehaviour
     public void ExitApp()
     {
         Application.Quit();
+    }
+
+    [ContextMenu("Populate List of names")]
+    private void PopulateNames()
+    {
+        scenesInBuildNames.Clear();
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string name = System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+            scenesInBuildNames.Add(name);
+        }
     }
 }
