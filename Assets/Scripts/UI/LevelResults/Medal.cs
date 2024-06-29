07aa825674cdb71691d6969af4f2e00dd4e0199a
Nicodragon25 <nicolasariellaure@gmail.com>
2024-06-29 01:32:36 -0300

    Add medal animation

---
+++ Assets/Scripts/UI/LevelResults/Medal.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Medal : MonoBehaviour
+{
+    private float originalSize = 0;
+    [SerializeField] private float InitialSizeMultiplier;
+    [SerializeField] private float animDuration;
+    private RectTransform rectTransform;
+
+    [SerializeField] private UnityAction OnAnimFinished;
+    void OnEnable()
+    {
+        rectTransform = this.GetComponent<RectTransform>();
+        originalSize = rectTransform.localScale.x;
+        rectTransform.localScale = new Vector3(originalSize * InitialSizeMultiplier, originalSize * InitialSizeMultiplier);
+
+        StartCoroutine(MedalAnimation());
+    }
+
+    private IEnumerator MedalAnimation()
+    {
+        float startTime = Time.time;
+        float timer = 0;
+        while (timer < animDuration)
+        {
+            timer = Time.time - startTime;
+            float size = Mathf.Lerp(originalSize * InitialSizeMultiplier, originalSize, timer / animDuration);
+            rectTransform.localScale = new Vector3(size, size);
+            yield return null;
+        }
+        OnAnimFinished?.Invoke();
+    }
+}
