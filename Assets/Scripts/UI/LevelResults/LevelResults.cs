c8ceec8c34bcdc22e8ef4d52f946fbacbb4bfe5e
Nicodragon25 <nicolasariellaure@gmail.com>
2024-06-29 00:26:39 -0300

    Add panel duration and time text setting up

--- Assets/Scripts/UI/LevelResults/LevelResults.cs
+++ Assets/Scripts/UI/LevelResults/LevelResults.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelResults : MonoBehaviour
 {
     [SerializeField] private List<GameObject> tierObjects = new List<GameObject>();
-
+    [SerializeField] private float panelDuration;
     [HideInInspector] public int tiersAchieved = 0;
     protected virtual void OnEnable()
     {
@@ -13,5 +13,19 @@ public class LevelResults : MonoBehaviour
         {
             tierObjects[i].SetActive(true);
         }
+
+        StartCoroutine(TurnOffPanel());
+    }
+
+    private IEnumerator TurnOffPanel()
+    {
+        float startTime = Time.time;
+        float timer = 0;
+        while (timer < panelDuration)
+        {
+            timer = Time.time - startTime;
+            yield return null;
+        }
+        gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/UI/LevelResults/TimedLevelResult.cs
+++ Assets/Scripts/UI/LevelResults/TimedLevelResult.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimedLevelResult : LevelResults
 {
     [SerializeField] private float tier1Time;
     [SerializeField] private float tier2Time;
     [SerializeField] private float tier3Time;
+    [SerializeField] private TextMeshProUGUI finalTimeText;
+    [SerializeField] private TextMeshProUGUI bestTimeText;
 
     [HideInInspector] public float levelTime;
+    [HideInInspector] public float bestTime;
     protected override void OnEnable()
     {
         if (levelTime < tier3Time)
@@ -20,6 +24,18 @@ public class TimedLevelResult : LevelResults
         else
             tiersAchieved = 0;
 
+        SetTexts();
         base.OnEnable();
+    }
+
+    private void SetTexts()
+    {
+        int minutes = Mathf.FloorToInt(levelTime) / 60;
+        int seconds = Mathf.FloorToInt(levelTime) % 60;
+        finalTimeText.text = "Final Time: " + minutes + ":" + seconds;
+
+        minutes = Mathf.FloorToInt(bestTime) / 60;
+        seconds = Mathf.FloorToInt(bestTime) % 60;
+        bestTimeText.text = "Best Time: " + minutes + ":" + seconds;
     }
 }
