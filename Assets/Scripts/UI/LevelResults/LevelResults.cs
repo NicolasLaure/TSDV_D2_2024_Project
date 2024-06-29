960e1155fe1e96fef23b19f37369a8ddd256e5de
Nicodragon25 <nicolasariellaure@gmail.com>
2024-06-29 01:09:26 -0300

    Add delay between medals

--- Assets/Scripts/UI/LevelResults/LevelResults.cs
+++ Assets/Scripts/UI/LevelResults/LevelResults.cs
@@ -6,17 +6,21 @@ public class LevelResults : MonoBehaviour
 {
     [SerializeField] private List<GameObject> tierObjects = new List<GameObject>();
     [SerializeField] private float panelDuration;
+    [SerializeField] private float timeBetweenMedals;
     [HideInInspector] public int tiersAchieved = 0;
     protected virtual void OnEnable()
+    {
+        StartCoroutine(ShowMedals());
+        StartCoroutine(TurnOffPanel());
+    }
+    private IEnumerator ShowMedals()
     {
         for (int i = 0; i < tiersAchieved; i++)
         {
             tierObjects[i].SetActive(true);
+            yield return new WaitForSeconds(timeBetweenMedals);
         }
-
-        StartCoroutine(TurnOffPanel());
     }
-
     private IEnumerator TurnOffPanel()
     {
         float startTime = Time.time;
